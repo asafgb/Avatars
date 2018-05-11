09a1fe0275f37caa3b96d9773f607702398ecb6c
asafgb <asafshazar@gmail.com>
2018-05-11 15:27:27 +0300

    Test Bottoms

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,17 @@
 import React, { Component } from 'react';
 import logo from  '../images/logo.svg'; //'./images/logo.svg';
 import './App.css';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import ButtonAppBar  from '../components/Avatars_TopNavBar';
 import MiniDrawer from '../components/Avatars_SideNavBar';
-
+import BottomNavigationExampleSimple from '../components/Avatars_BottomNavigation'
 
 
 class App extends Component {
   render() {
     return (
-      /*<div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <MuiThemeProvider>
-          <RaisedButtonExampleSimple />
-        </MuiThemeProvider>
-      </div>    */
       <div className="App"  >
 
          <MiniDrawer title ="Choose Your Avatar"  />
-         
-           {/* <ButtonAppBar title ="Choose Your Avatar" />  */}
-           
+             {/* <BottomNavigationExampleSimple/> */}
       </div>
     );
   }
